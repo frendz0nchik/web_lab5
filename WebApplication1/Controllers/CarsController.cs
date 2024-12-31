7503ae8a20a2f25e4cde7046bd2b9003d24f9cdb
SystemX <SystemX@DESKTOP-DI00305>
2024-12-31 15:49:54 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarRepository _repository;
+        private readonly RabbitMQService _rabbitMQService;
+
+        public CarsController(CarContext context)
+        {
+            _repository = new CarRepository(context);
+            _rabbitMQService = new RabbitMQService();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> Get()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> Get(int id)
+        {
+            var car = await _repository.GetByIdAsync(id);
+            if (car == null)
+                return NotFound();
+            return car;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Car>> Post(Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+            await _repository.AddAsync(car);
+
+            // Генерация события
+            var carEvent = new CarEvent { EventType = "CREATE", Car = car };
+            _rabbitMQService.PublishEvent(carEvent);
+
+            return Ok(car);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Car>> Put(Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+            if (await _repository.GetByIdAsync(car.car_id) == null)
+                return NotFound();
+
+            await _repository.UpdateAsync(car);
+
+            // Генерация события
+            var carEvent = new CarEvent { EventType = "UPDATE", Car = car };
+            _rabbitMQService.PublishEvent(carEvent);
+
+            return Ok(car);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Car>> Delete(int id)
+        {
+            var car = await _repository.GetByIdAsync(id);
+            if (car == null)
+                return NotFound();
+
+            await _repository.DeleteAsync(id);
+
+            // Генерация события
+            var carEvent = new CarEvent { EventType = "DELETE", Car = car };
+            _rabbitMQService.PublishEvent(carEvent);
+
+            return Ok(car);
+        }
+    }
+}
---
+++ WebApplication1/Services/RabbitMQService.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace WebApplication1.Services
+{
+    public class RabbitMQService
+    {
+        private readonly string _exchangeName = "cars_events_exchange";
+        private readonly string _queueName = "cars_events_queue";
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitMQService()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            // Объявляем Exchange и Queue
+            _channel.ExchangeDeclare(exchange: _exchangeName, type: ExchangeType.Fanout);
+            _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueBind(queue: _queueName, exchange: _exchangeName, routingKey: "");
+        }
+
+        public void PublishEvent(object message)
+        {
+            var jsonMessage = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(jsonMessage);
+
+            _channel.BasicPublish(exchange: _exchangeName, routingKey: "", basicProperties: null, body: body);
+        }
+
+        public void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}
